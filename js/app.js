402d6c04495543af7fee047c06adf616b33575d9
Yixin Peng <ypeng27@hawk.iit.edu>
2020-09-01 15:49:21 -0500

    Added author and subtitle

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var author_id_global;
 var author_name_global;
-var author_image_url_global;
+var author_icon_global;
 var paragraph_number = 1;
 
 async function initFirebase() {
@@ -35,7 +35,7 @@ async function fetchUserProfile() {
     try {
         var doc = await firebase.firestore().collection("users").doc(author_id_global).get()
         author_name_global = doc.get("name");
-        author_image_url_global = doc.get("image_url")
+        author_icon_global = doc.get("image_url")
     } catch (error) {
         alert(error.message);
     }
@@ -49,30 +49,52 @@ async function submitArticle() {
     var title = document.getElementById('inputTitle').value;
     var description = document.getElementById('inputDescription').value;
     var image_url = document.getElementById('inputImageUrl').value;
+    var author_name = document.getElementById('inputAuthorName').value;
+    var author_icon = document.getElementById('inputAuthorIcon').value;
     var content = [];
     for (var i = 1; i <= paragraph_number; i++) {
-        var doc = document.getElementById('inputParagraph' + i).value.trim();
-        if (doc !== "")
-            content.push({ body: doc });
+        var subtitle = document.getElementById('inputSubtitle' + i).value.trim();
+        var body = document.getElementById('inputParagraph' + i).value.trim();
+        if (body !== "")
+            content.push({ subtitle: subtitle, body: body });
+
     }
     var data = {
         title: title,
         description: description,
         author_uid: author_id_global,
-        content: content,
         created_date: firebase.firestore.Timestamp.now(),
         verified: false
     };
-    if (author_name_global)
-        data.author_name = author_name_global;
-    if (author_image_url_global)
-        data.icon = author_image_url_global;
+    if (author_name.trim() !== "") {
+        data.author_name = author_name;
+    } else {
+        if (author_name_global)
+            data.author_name = author_name_global;
+    }
+    if (author_icon.trim() !== "") {
+        data.icon = author_icon;
+    } else {
+        if (author_icon_global)
+            data.icon = author_icon_global;
+    }
     if (image_url.trim() !== "") {
         data.image_url = image_url;
     }
     console.log(data);
+    console.log(content);
+    await addArticleInFirebase(data, content);
+}
+
+async function addArticleInFirebase(data, content) {
     try {
-        var docRef = await firebase.firestore().collection("articles").add(data)
+        var db = firebase.firestore();
+        var docRef = await db.collection("articles").add(data);
+        var batch = db.batch();
+        content.forEach((doc) => {
+            batch.set(db.collection("articles").doc(docRef.id).collection('content').doc(), doc);
+        });
+        await batch.commit();
         alert("Document Saved!");
         console.log("Document written with ID: ", docRef.id);
     } catch (error) {
@@ -108,12 +130,20 @@ function addParagraph() {
     paragraph_number++;
     var ul = document.getElementById("listParagraph");
     var li = document.createElement("li");
+
+    var input = document.createElement("input");
+    input.setAttribute("class", "form-control subtitle");
+    input.setAttribute("id", "inputSubtitle" + paragraph_number);
+    input.setAttribute("placeholder", "Subtitle " + paragraph_number + " (Optional)");
+    li.appendChild(input);
+
     var textarea = document.createElement("textarea");
     textarea.setAttribute("class", "form-control paragraph");
     textarea.setAttribute("rows", "3");
     textarea.setAttribute("id", "inputParagraph" + paragraph_number);
     textarea.setAttribute("placeholder", "Paragraph " + paragraph_number + " (Optional)");
     li.appendChild(textarea);
+
     li.setAttribute("id", "element" + paragraph_number); // 
     ul.appendChild(li);
 }
